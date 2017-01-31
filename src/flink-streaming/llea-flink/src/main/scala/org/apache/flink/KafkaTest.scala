f546fd1d5fe968e70e3df8ce7773a95e6fb2f6fc
Ingolf Warnke <ingolf.warnke@gmail.com>
2017-01-31 02:56:02 +0000

    Add redis connector prototype

--- src/flink-streaming/llea-flink/src/main/scala/org/apache/flink/KafkaTest.scala
+++ src/flink-streaming/llea-flink/src/main/scala/org/apache/flink/KafkaTest.scala
@@ -5,28 +5,59 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09
-// don't ask brennan about this later
+// Redis Connector
+import org.apache.flink.streaming.connectors.redis.RedisSink
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
 // import org.apache.flink.streaming.connectors.kafka.api.KafkaSink
+
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+//import utils.{ConfigurationManager}
+//import src.main.scala.utils.{LleaRedisMapper}
+
+//class LleaRedisMapper extends RedisMapper[(String, String)]{
+class LleaRedisMapper extends RedisMapper[String]{
+  override def getCommandDescription: RedisCommandDescription = {
+    new RedisCommandDescription(RedisCommand.HSET, "HASH_NAME")
+    // new RedisCommandDescription(RedisCommand.HSET, "pipeline")
+  }
+  //override def getKeyFromData(data: (String, String)): String = ("asdf", "qwert)" //data._1
+  //override def getValueFromData(data: (String, String)): String = "123" //data._2
+  override def getKeyFromData(data: (String)): String = "qwer" //data._1
+  override def getValueFromData(data: (String)): String = "asdf" //data._2
+}
 
 object Main {
   def main(args: Array[String]) {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val properties = new Properties()
-    println("hello")
+    // println("hello")
     // use public dns of first worker, it should be the one where kafka runs on
     properties.setProperty("bootstrap.servers", "ec2-35-167-63-252.us-west-2.compute.amazonaws.com:9092")
     properties.setProperty("zookeeper.connect", "ec2-35-167-63-252.us-west-2.compute.amazonaws.com:2181")
     properties.setProperty("group.id", "org.apache.flink")
+
     println("hello")
+
+    // Configure Flink to Redis connector
+    // val conf = new FlinkJedisPoolConfig.Builder().setHost(config.get("redis.host")).build()
+    val conf = new FlinkJedisPoolConfig.Builder().setHost("52.25.10.79").build()
+
     val stream = env
       .addSource(new FlinkKafkaConsumer09[String]("pipeline", new SimpleStringSchema(), properties))
-      .print
-     // .addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-to-kafka", new SimpleStringSchema()))
+     //.addSink(new RedisSink[String](conf, new FlinkRedisMapper))
+     //.addSink(new RedisSink[String](conf, new LleaRedisMapper))
+     //.addSink(new RedisSink[(String, String)](conf, new LleaRedisMapper))
+     //.addSink(new RedisSink[(String, String)](conf, new LleaRedisMapper))
+     .addSink(new RedisSink[(String)](conf, new LleaRedisMapper))
+     //.print <-works
+     //.addSink(new FlinkKafkaProducer09[String]("localhost:9092", "flink-to-kafka", new SimpleStringSchema()))    <- works
+     // stream.addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-to-rethinkdb", new SimpleStringSchema()))
+     // val x =  stream.addSink(new KafkaSink("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-output", new MySimpleStringSchema))
+     //  println("hello")
 
-   /* stream.addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-to-rethinkdb", new SimpleStringSchema()))
-*/
-/*    val x =  stream.addSink(new KafkaSink("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-output", new MySimpleStringSchema))
-  */  println("hello")
     env.execute("Flink Kafka Example")
   }
 }
