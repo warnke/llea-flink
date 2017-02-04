48f49a9dcfd9347bb2e19c6f670e58ed13e1b68f
ingolf <ingolf.warnke@gmail.com>
2017-02-03 17:11:19 -0800

    minor producer refactor (tabstops...)

--- src/python-producer/producer.py
+++ src/python-producer/producer.py
@@ -14,7 +14,7 @@ def eprint(*args, **kwargs):
 
 class ModPartitioner(partitioners.BasePartitioner):
     """
-    Returns a the int value of the key mod the number of partitions
+    Returns int value of the key mod the number of partitions
     """
     def __call__(self, partitions, key):
         """
@@ -35,68 +35,67 @@ class ModPartitioner(partitioners.BasePartitioner):
 mod_partitioner = ModPartitioner()
 
 class Producer(threading.Thread):
-  daemon = True
-
-  def run(self):
-    client = KafkaClient("localhost:9092")
-    topic = client.topics["pipeline"]
-    producer = topic.get_producer(partitioner=mod_partitioner, linger_ms = 200)
-
-    keyNum = 30
-    key = list(np.arange(keyNum))
-
-    idNum = 10000
-    id = list(np.arange(idNum))
-
-    ratepersecond = 5000
-    burstFraction=0.1
-    burstnum = ratepersecond*burstFraction
-
-    ## Gaussian parameters for variation of message rate
-    gmu = 0.0
-    gsig = 0.1
-
-    ## Lognormal parameters for delay of message delivery
-    lmu = 2.5
-    lsig = 1.0
-
-    global totalSamples
-
-    while True:
-      ## emit data in bursts, compute number of messages in this burst
-      ## from target rate
-      currNum = int(round( burstnum*(1 + np.random.normal(gmu, gsig)) ))
-      tt = time.time()
-      totalSamples += currNum
-      for sample in xrange(currNum):
-        delay = np.random.lognormal(lmu, lsig)
-        currKey = key[np.random.randint(keyNum)]
-        currID = id[np.random.randint(idNum)]
-	## Emulate delayed message delivery by back dating event time
-        eventTime = tt - delay
-	outputStr = "%s;%s;%s" % (currKey, currID, eventTime)
-        #print(outputStr)
-        producer.produce(outputStr, partition_key=str(currKey))
-        ## Every ten minutes, bump rate for two minutes to trigger alert.
-	## Trigger if event time falls into the (key%5)-th 2 min interval of 10 min
-	## rolling cycle.
-	## To get rate increase by 1/n, trigger if `not np.random.randint(n)`.
-        if ( not np.random.randint(3) and 120*(currKey%5) <= (eventTime % 600) < 120*(currKey%5 + 1) ):
-          outputStr = "%s;%s;%s" % (currKey, currID, eventTime + np.random.normal(0.0, 5.0))
-          producer.produce(outputStr, partition_key=str(currKey))
-
-      timeSpent = time.time() - tt
-      sleepTime = burstFraction - timeSpent
-      if sleepTime < 0:
-        print("Rate too high! Launch more producers with smaller rate each.")
-      else:
-        time.sleep(sleepTime)
+    daemon = True
+  
+    def run(self):
+        client = KafkaClient("localhost:9092")
+        topic = client.topics["pipeline"]
+        producer = topic.get_producer(partitioner=mod_partitioner, linger_ms = 200)
+  
+        keyNum = 30
+        key = list(np.arange(keyNum))
+  
+        idNum = 10000
+        id = list(np.arange(idNum))
+  
+        ratepersecond = 5000
+        burstFraction=0.1
+        burstnum = ratepersecond*burstFraction
+  
+        ## Gaussian parameters for variation of message rate
+        gmu = 0.0
+        gsig = 0.1
+  
+        ## Lognormal parameters for delay of message delivery
+        lmu = 2.5
+        lsig = 1.0
+  
+        global totalSamples
+  
+        while True:
+            ## emit data in bursts, compute number of messages in this burst
+            ## from target rate
+            currNum = int(round( burstnum*(1 + np.random.normal(gmu, gsig)) ))
+            tt = time.time()
+            totalSamples += currNum
+            for sample in xrange(currNum):
+                delay = np.random.lognormal(lmu, lsig)
+                currKey = key[np.random.randint(keyNum)]
+                currID = id[np.random.randint(idNum)]
+                ## Emulate delayed message delivery by back dating event time
+                eventTime = tt - delay
+                outputStr = "%s;%s;%s" % (currKey, currID, eventTime)
+                #print(outputStr)
+                producer.produce(outputStr, partition_key=str(currKey))
+                ## Every ten minutes, bump up rate for two minutes to trigger alert.
+                ## Trigger if event time falls into the (key%5)-th 2 min interval of 10 min rolling cycle.
+                ## To get rate increase by 1/n, trigger if `not np.random.randint(n)`.
+                if ( not np.random.randint(3) and 120*(currKey%5) <= (eventTime % 600) < 120*(currKey%5 + 1) ):
+                    outputStr = "%s;%s;%s" % (currKey, currID, eventTime + np.random.normal(0.0, 5.0))
+                    producer.produce(outputStr, partition_key=str(currKey))
+  
+            timeSpent = time.time() - tt
+            sleepTime = burstFraction - timeSpent
+            if sleepTime < 0:
+                print("Rate too high! Launch more producers with smaller rate each.")
+            else:
+                time.sleep(sleepTime)
 
 
 startTime = time.time()
 totalSamples = 0
 Producer().start()
 while True:
-  time.sleep(3)
+    time.sleep(3)
 endTime = time.time()
 eprint(totalSamples, " samples produced in ", endTime - startTime, " seconds.")
