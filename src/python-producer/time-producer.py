fdca7a124dbd2e65cb589612d62f31a00da5433d
ingolf <ingolf.warnke@gmail.com>
2017-02-04 21:25:23 -0800

    time-producer works! Auxiliary little script that adds current worker timestamps over which we'll average later.

--- src/python-producer/time-producer.py
+++ src/python-producer/time-producer.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 ## printing to stderr in python3 style
 from __future__ import print_function
+import redis
 import sys
 
 import logging, time
-
 #import numpy as np
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+redis_host = '52.25.10.79'
+db_client = redis.StrictRedis(host=redis_host, password=None)
+
 count = 0
 while True:
-    tt = time.time()
-    count = count + 1 if count < 3 else 1
-    print("currtime-{}".format(count), '{:.2f}'.format(time.time()))
-    time.sleep(1)
+    db_client.set("currtime-{}".format(count), '{:.2f}'.format(time.time()))
+    print("Connect to redis on {}. SET: ".format(redis_host), "currtime-{}".format(count), '{:.2f}'.format(time.time()))
 
+    count = count + 1 if count < 2 else 0
+    time.sleep(1)
